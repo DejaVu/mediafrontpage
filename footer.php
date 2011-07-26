d367b423f8dff3bab454c0804958de1432206ab7
DejaVu77 <leonsmy@googlemail.com>
2011-07-26 12:44:06 +0100

    Linked sites now have the Title

--- footer.php
+++ footer.php
@@ -1,30 +1,17 @@
 <link href="/css/footer.css" rel="stylesheet" type="text/css">
-<?php
-include "config.php";
-if(!empty($subnavlink)||!empty($subnavlink_blank)||!empty($subnavselect)){
-	echo "<div style=\"width:100%; position: fixed; bottom: 0px;\" id='footer'>";
-	//echo "<br> ";
-	echo "<ul>";
-
-	if(!empty($subnavlink)){
-		foreach( $subnavlink as $navlinklabel => $navlinkpath) {
-			echo "<li><a href='programs.php?p=".$navlinkpath."'>".$navlinklabel."</a></li>";
+<?php 
+	include "config.php";
+		if(!empty($subnavlink)||!empty($subnavlink_blank)||!empty($subnavselect)){
+			echo "<div style=\"width:100%; position: fixed; bottom: 0px;\" id='footer'>";
+			echo "<ul>";
+		
+		if(!empty($subnavlink)){
+			foreach( $subnavlink as $navlinklabel => $navlinkpath) {
+			echo "<li><a href='programs.php?p=".$navlinkpath."&title=".$navlinklabel."'>".$navlinklabel."</a></li>";
 		}
 	}
-
-/*
-	if(!empty($subnavselect)){
-		echo "<li><select onchange=\"top.frames['main'].location.href = this.value;\">";
-		echo "<option value='mediafrontpage.php' selected></option>";
-		foreach($subnavselect as $navselectlabel => $navselectpath){
-			echo "<option value='$navselectpath'>".$navselectlabel."</option>";
-		}
-		echo "</select></li>";
+			echo "</ul>";
+			echo "</div>";
+			echo "</div>";	
 	}
-*/
-
-	echo "</ul>";
-	echo "</div>";
-	echo "</div>";	
-}
 ?>
--- programs.php
+++ programs.php
@@ -1,54 +1,55 @@
 <?php 
-include "nav.php"; 
-require_once "config.php";
-$qs = $_GET['p']
+	include "nav.php";
+	require_once "config.php";
+		$qs = $_GET['p'];
+		$title = (isset($_GET['title'])) ? $_GET['title'] : $qs;
 ?>
 <html style="overflow:none">
 	<head>
-  <title>MediaFrontPage - <?php echo $qs ?></title>
-	<link rel="shortcut icon" href="favicon.ico" />
-  <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
-	<link rel="stylesheet" type="text/css" href="css/widget.css" />
-	<link rel="stylesheet" type="text/css" href="css/front.css" />
-  <link rel="stylesheet" type="text/css" href="css/static_widget.css" />
+		<title>MediaFrontPage - <?php echo $qs ?></title>
+		<link rel="shortcut icon" href="favicon.ico" />
+		<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+		<link rel="stylesheet" type="text/css" href="css/widget.css" />
+		<link rel="stylesheet" type="text/css" href="css/front.css" />
+		<link rel="stylesheet" type="text/css" href="css/static_widget.css" />
 	</head>
-  <body><br>
-    <center>    
-			<table width="98%" height="95%" align="center" cellpadding="0" cellspacing="0" class="widget">
-				<tr style="cursor: move; ">
-					<td align=center colspan=2 height=25>
-		        <div class="widget-head"><h3><?php echo $qs; ?></h3></div>
-				    </td>
-						<tr>
-							<td><br>
-							  <iframe class='widget' src='<?php 
-if ($qs == 'SickBeard') {
-	echo $sickbeardurl;
-}	elseif($qs == 'CouchPotato') {
-	echo $cp_url;
-}	elseif($qs == 'SabNZBd') {
-	echo $saburl;
-}	elseif($qs == 'Transmission') {
-	echo $transmission_web;
-}	elseif($qs == 'uTorrent') {
-	echo $utorrent_url;
-}	elseif($qs == 'XBMC') {
-	echo str_replace("vfs/","",$xbmcimgpath);
-}	elseif($qs == 'HeadPhones') {
-	echo $headphones_url;
-}	elseif($qs == 'JDownloader') {
-	echo $jd_weburl;
-}	elseif($qs == 'SubSonic') {
-	echo $subsonic_url;
-}	else {
-	echo $qs;
-}
-?>'; frameborder='0' height='96%' width='98%' scrolling='auto'></iframe>
-						</td>
-			    </tr>
-			  </tr>
+  <body><br><center>    
+	<table width="98%" height="95%" align="center" cellpadding="0" cellspacing="0" class="widget">
+		<tr style="cursor: move; ">
+			<td align=center colspan=2 height=25>
+		        <div class="widget-head">
+					<h3><?php echo $title; ?></h3>
+				</div>
+			</td>
+		<tr>
+			<td><br>
+				<iframe class='widget' src='<?php if ($qs == 'SickBeard') {
+													echo $sickbeardurl;
+											}	elseif($qs == 'CouchPotato') {
+													echo $cp_url;
+											}	elseif($qs == 'SabNZBd') {
+													echo $saburl;
+											}	elseif($qs == 'Transmission') {
+													echo $transmission_web;
+											}	elseif($qs == 'uTorrent') {
+													echo $utorrent_url;
+											}	elseif($qs == 'XBMC') {
+													echo str_replace("vfs/","",$xbmcimgpath);
+											}	elseif($qs == 'HeadPhones') {
+													echo $headphones_url;
+											}	elseif($qs == 'JDownloader') {
+													echo $jd_weburl;
+											}	elseif($qs == 'SubSonic') {
+													echo $subsonic_url;
+												} else {
+													echo $qs;
+											}?>'; frameborder='0' height='96%' width='98%' scrolling='auto'></iframe>
+			</td>
+	    </tr>
       </table>
-	    <?php include "footer.php"; ?>
+<?php
+	include "footer.php"; 
+?>
     </center>
   </body>
 </html>
