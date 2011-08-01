618ee4cab2fa7dda71cbfee7d4eac76e3fac020d
DejaVu77 <leonsmy@googlemail.com>
2011-08-01 03:07:20 +0100

    Added SubSonic Webroot Settings (Hopefully)

--- config.php
+++ config.php
@@ -374,6 +374,9 @@ if($REVERSE_PROXY){
      if(!empty($HEADPHONES_WEBROOT)){
           $headphones_url = 'http://'.$GLOBAL_IP.'/'.$HEADPHONES_WEBROOT.'/home/';
      }
+     if(!empty($SUBSONIC_WEBROOT)){
+          $subsonic_url = 'http://'.$GLOBAL_IP.'/'.$SUBSONIC_WEBROOT.'/login.view?user=$SUBSONIC_USERNAME&password=$SUBSONIC_PASS/';
+     }
  } else {
    $xbmcjsonservice        = "http://$xbmclogin"."$XBMC_IP:$XBMC_PORT/jsonrpc";
    $xbmcimgpath            = "http://$xbmclogin"."$XBMC_IP:$XBMC_PORT/vfs/";
