b31ffde3d54bf67841b1fe3eea7e1130c8f2367a
DejaVu77 <leonsmy@googlemail.com>
2011-08-13 11:40:13 +0100

    Compare current with update (Git Compare link)

--- settings.php
+++ settings.php
@@ -61,23 +61,23 @@
 <html>
      <head>
      <title>MediaFrontPage - Settings</title>
-     <link href="css/front.css" rel="stylesheet" type="text/css">
-     <link href="css/settings.css" rel="stylesheet" type="text/css">
-     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-     <script type="text/javascript" src="js/fisheye-iutil.min.js"></script>
-     <script type="text/javascript" src="js/settings.js"></script>
+     <link rel="stylesheet" type="text/css" href="css/front.css">
+     <link rel="stylesheet" type="text/css" href="css/settings.css">
      <link rel="stylesheet" type="text/css" href="css/widget.css">
      <link rel="stylesheet" type="text/css" href="css/static_widget.css">
      <link rel="stylesheet" type="text/css" href="css/footer.css">
-     <script src="js/jquery.scrollTo-1.3.3-min.js" type="text/javascript"></script>
-     <script src="js/jquery.localscroll-1.2.5-min.js" type="text/javascript"></script>
-     <script src="js/jquery.serialScroll-1.2.1-min.js" type="text/javascript"></script>
-     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
      <link rel="stylesheet" type="text/css" href="css/jquery.pnotify.default.css">
      <link rel="stylesheet" type="text/css" href="css/UI/jquery-ui-1.8.14.custom.css">
+     <link rel="stylesheet" type="text/css" href="css/tipsy.css"/>
+     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+     <script type="text/javascript" src="js/fisheye-iutil.min.js"></script>
+     <script type="text/javascript" src="js/settings.js"></script>
+     <script type="text/javascript" src="js/jquery.scrollTo-1.3.3-min.js"></script>
+     <script type="text/javascript" src="js/jquery.localscroll-1.2.5-min.js"></script>
+     <script type="text/javascript" src="js/jquery.serialScroll-1.2.1-min.js"></script>
+     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
      <script src="js/jquery.pnotify.js" type="text/javascript"></script>
      <script type='text/javascript' src='js/jquery.tipsy.js'></script>
-     <link rel="stylesheet" href="css/tipsy.css" type="text/css" />
      <script type='text/javascript'>
      $(function() {
           $('input').tipsy({gravity: 'w', fade: true});
@@ -161,7 +161,8 @@
                   <td colspan="2">
                   <?php 
                     if($commitNo != $currentVersion){
-                          echo "<input type='button' value='Update Available' Title='Click To Update' onclick=\"location.href='update.php'\" />";
+                          echo "<p>There is an <a href='https://github.com/DejaVu/mediafrontpage/compare/".$currentVersion."...master' Title='Compare the new changes' target='_blank'>new version available </a>(or else your ahead of the master)</p><br>";
+                          echo "<input type='button' value='Update Now' Title='Click To Update' onclick=\"location.href='update.php'\" />";
                      }else{
                           echo "Congratulations, your on the current version of MediaFrontPage.";
                               } 
